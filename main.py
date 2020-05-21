f844755633a2bc16f45707c668b81613fac459e8
aliprf <aliprf@gmail.com>
2020-05-21 17:09:02 -0600

    stack+mobilenet --> mnv2_hm_r_v2 added to model

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
                     # weight='weights-145-0.00011.h5',
                     weight=None,
                     train_on_batch=False,
-                    accuracy=90)
+                    accuracy=100)
 
 
 
