f5b972395bdbe08b93df82bf1bfb57ba993cd662
aliprf <aliprf@gmail.com>
2020-06-25 13:40:58 -0600

    train cofw with TF_95% is ok

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     #
 
     trainer = Train(use_tf_record=True,
-                    dataset_name=DatasetName.cofw,
+                    dataset_name=DatasetName.ibug,
                     custom_loss=False,
                     arch='efficientNet',
                     # arch='mnv2_hm_r_v2',
