d13c6d7e22dfd609da7beac292e69e0df3e679e6
aliprf <aliprf@gmail.com>
2020-07-15 02:37:41 -0600

    train ibug teacherNet

--- configuration.py
+++ configuration.py
@@ -57,7 +57,7 @@ class LearningConfig:
     MAX_LR = 1e-2
     STEP_SIZE = 10
     # batch_size = 80
-    batch_size = 30
+    batch_size = 45
     steps_per_validation_epochs = 5
 
     epochs = 200
