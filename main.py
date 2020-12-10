b9362bdb83395f49721af773cd43da9c576c09f4
aliprf <aliprf@gmail.com>
2020-12-09 22:15:45 -0700

    train cofw

--- main.py
+++ main.py
@@ -97,9 +97,11 @@ if __name__ == '__main__':
                      weight_path_student='./teacher_models/ds_cofw_mn_base.h5',
                      # weight_path_student=None,
                      loss_weight_student=2.0,
-                     arch_tough_teacher='efficientNet', weight_path_tough_teacher='./teacher_models/ds_cofw_ef_100.h5',
+                     arch_tough_teacher='efficientNet', weight_path_tough_teacher=None,
+                     # arch_tough_teacher='efficientNet', weight_path_tough_teacher='./teacher_models/ds_cofw_ef_100.h5',
                      loss_weight_tough_teacher=0.80,
-                     arch_tol_teacher='efficientNet', weight_path_tol_teacher='./teacher_models/ds_cofw_ef_95.h5',
+                     arch_tol_teacher='efficientNet', weight_path_tol_teacher=None,
+                     # arch_tol_teacher='efficientNet', weig/ht_path_tol_teacher='./teacher_models/ds_cofw_ef_95.h5',
                      loss_weight_tol_teacher=0.60)
 
     '''Cofw dataset: this ds is not normal for both mn_base and efn100'''
