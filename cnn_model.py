d0bfe2be66cc339e1bc0eff5b73a961763e03de4
aliprf <aliprf@gmail.com>
2020-12-12 22:53:35 -0700

    train wflw

--- cnn_model.py
+++ cnn_model.py
@@ -108,7 +108,7 @@ class CNNModel:
         # x = mobilenet_model.get_layer('global_average_pooling2d_2').output  # 1280
         x = mobilenet_model.get_layer('global_average_pooling2d').output  # 1280
         # x = Dropout(0.5)(x)
-        out_landmarks = Dense(output_len, name='O_L')(x)
+        out_landmarks = Dense(output_len, use_bias=False, name='O_L')(x)
         inp = mobilenet_model.input
 
         revised_model = Model(inp, out_landmarks)
